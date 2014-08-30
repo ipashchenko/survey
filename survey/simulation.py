9f4a0fdf6dd6fb1376d4703a6b321f7d1f05089b
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-08-30 17:44:33 +0400

    working on

--- survey/simulation.py
+++ survey/simulation.py
@@ -45,7 +45,7 @@ def detection_fraction(mu_loga, std_loga, beta_e, mu_logs, std_logs, bsls,
                        alpha_e=2., s_thr=0.05, size=10 ** 5):
     """
     Returns detection fraction for distribution of source sizes with log(major
-    axis) ~ N(mu_loga, std_loga), minor-to-major axis distribution ~
+    axis) ~ N(loga, std_loga), minor-to-major axis distribution ~
     Beta(alpha_e, beta_e), log(flux) at zero (u,v)-point distribution ~
     N(mu_logS, tau_logS) and baseline length pdf ``bsl_pdf`` (e.g. from kernel
     estimate).
@@ -61,7 +61,7 @@ def detection_fraction(mu_loga, std_loga, beta_e, mu_logs, std_logs, bsls,
         Std of normal distribution for logs, where s [Jy].
     :param bsls:
         Baselines. Array-like with size=``size``. If it is not, then resample
-        to ``size.
+        to ``size``.
     :param alpha_e (optional):
         Parameter of Beta-distribution for axis ratios. Default = 2.
     :param s_thr:
@@ -80,24 +80,104 @@ def detection_fraction(mu_loga, std_loga, beta_e, mu_logs, std_logs, bsls,
     if not len(bsls) == size:
         bsls = np.random.choice(bsls, size=size)
     pa = np.random.unif(0., np.pi, size=size)
-    fluxes = flux(bsl, pa, s, a, e)
+    fluxes = flux(bsls, pa, s, a, e)
     n_det = len(np.where(fluxes > s_thr)[0])
     return float(n_det) / size
 
 
+# TODO: First, use the same ``loga``, ``beta_e`` (geometry) & ``logs`` for all
+# sources, the the same geometry but distribution of ``logs`` and then
+# distributions for all.
 class Simulation(object):
-    def __init__(self, mu_loga, mu_logs, beta_e, bsls_list, alpha_e=2.):
+    def __init__(self, loga, logs, beta_e, bsls, alpha_e=2.):
         """
-        mu_loga - 2 values for min & max of mu_loga prior range.
-        mu_logs - 2 values for min & max of mu_logs prior range.
+        loga - 2 values for min & max of ``loga``/``mu_loga`` prior range.
+        logs - 2 values for min & max of ``logs``/``mu_logs`` prior range.
         beta - 2 values for min & max of beta prior range.
+        bsls - Array-like of baselines.
         """
-        self.mu_loga = mu_loga
-        self.mu_logs = mu_logs
+        self.loga = loga
+        self.logs = logs
         self.beta_e = beta_e
-        self.bsls_list = bsls_list
+        self.bsls = np.asarray(bsls)
         self.alpha_e = alpha_e
         self.p = list()
+
+    def run1(self, n_acc, fr_list, tol_list, bsls_borders=None, s_thr=0.05):
+        """
+        Run simulation with same values of ``loga``, ``beta_e`` & ``logs`` till
+        ``n_acc`` parameters are accepted.
+        :param n_acc:
+            Accepted number of parameters to stop simulation.
+        :param fr_list
+            Array-like of observed fractions.
+        :param tol_list:
+            Array-like of tolerances. The same length as ``fr_list``.
+        :param bsls_borders:
+            Array-like of borders for baseline ranges. Fractions will be
+            compared in intervals [bsls_borders[0], bsls_borders[1]],
+            [bsls_borders[1], bsls_borders[2]], ..., [bsls_borders[n-1],
+            bsls_borders[n]]. Length must be ``len(fr_list) + 1``.
+        :param s_thr:
+            Flux detection threshold.
+
+        :notes:
+            Size of samples used to count acceptance fractions is determined by
+            number of baselines used (``self.bsls``)
+
+        :return:
+            List of n lists of parameters [``loga``, ``beta_e``, ``logs``] that
+            give detection fractions equal to observed within tolerance.
+        """
+        # Assertions
+        assert(len(fr_list) == len(tol_list))
+        assert(len(fr_list) == len(bsls_borders) - 1)
+        # Initialize counting variable
+        n = 0
+        # Initialize accepted parameters container
+        parameters = list()
+
+        n_bsls = len(self.bsls)
+
+        fr_array = np.asarray(fr_list)
+        tol_array = np.asarray(tol_list)
+
+        # Partition baselines in ranges
+        bsls_partitioned = list()
+        for i in range(len(bsls_borders) - 1):
+            bsls_partitioned.append(np.where(self.bsls > bsls_borders[i] &
+                                             self.bsls < bsls_borders[i + 1]))
+        while n <= n_acc:
+            # Simulate ``n_bsls`` sources
+            # First simulate one value
+            loga = np.random.unif(self.loga[0], self.loga[1])
+            logs = np.random.unif(self.logs[0], self.logs[1])
+            beta_e = np.random.unif(self.beta_e[0], self.beta_e[1])
+            e = np.random.beta(self.alpha_e, beta_e)
+            # Save in list
+            params = [loga, beta_e, logs]
+            # Repeat to make ``n_bsls`` sources with the same parameters
+            loga = loga.repeat(n_bsls)
+            logs = logs.repeat(n_bsls)
+            beta_e = beta_e.repeat(n_bsls)
+            # Simulate ``n_bsls`` random positional angles for baselines
+            pa = np.random.unif(0., np.pi, size=n_bsls)
+            # For each range of baselines check summary statistics
+            for i, baselines in enumerate(bsls_partitioned):
+                n_ = len(baselines)
+                # Calculate detection fraction in this baseline range
+                fluxes = flux(baselines, pa[:n_], np.exp(logs[:n_]),
+                              np.exp(loga[:n_]), e[:n_])
+                n_det = len(np.where(fluxes > s_thr)[0])
+                det_fr = float(n_det) / n_
+                # If fail to get right fraction in this range then go to next
+                # loop of while
+                if abs(det_fr - fr_list[i]) > tol_list[i]:
+                    break
+            # If we got stuck here, then fractions in all baseline ranges are
+            # within tolerance of the observed.
+            n += 1
+            parameters.append(params)
 
     def run(self, n_acc, fr_list, tol_list, s_thr=0.05, size=10 ** 4.):
         """
@@ -109,15 +189,15 @@ class Simulation(object):
         n = 0
         while n < n_acc:
             # Sample from priors
-            mu_loga = np.random.unif(self.mu_loga[0], self.mu_loga[1])
+            mu_loga = np.random.unif(self.loga[0], self.loga[1])
             std_loga = np.random.gamma(0.1, 0.1)
-            mu_logs = np.random.unif(self.mu_logs[0], self.mu_logs[1])
+            mu_logs = np.random.unif(self.logs[0], self.logs[1])
             std_logs = np.random.gamma(0.1, 0.1)
             beta_e = np.random.unif(self.beta_e[0], self.beta_e[1])
             fractions = list()
             for fr in fr_list:
-                fractions.append([detection_fraction(self.mu_loga, self.std_loga,
-                                                self.beta_e, self.mu_logs,
+                fractions.append([detection_fraction(self.loga, self.std_loga,
+                                                self.beta_e, self.logs,
                                                 self.std_logs, bsls,
                                                 self.alpha_e, s_thr=s_thr,
                                                 size=size) for bsls in
