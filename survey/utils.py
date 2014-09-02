c16625f7073b985049d500f1f05a44ebb63814af
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-09-02 06:39:25 +0400

    Added hdi_of_icdf() function

--- survey/utils.py
+++ survey/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.optimize import fmin
 
 
 mas_to_rad = 4.85 * 10 ** (-9)
@@ -43,45 +44,24 @@ def get_samples_from_normal(mean, tau, size=10 ** 4):
     return np.random.normal(loc=mean, scale=1. / np.sqrt(tau), size=size)
 
 
-def detection_fraction(mu_loga, std_loga, beta_e, mu_logs, std_logs, bsls,
-                       alpha_e=2., s_thr=0.05, size=10 ** 5):
+def hdi_of_icdf(name, cred_mass=0.95, tol=1e-08, *args, **kwargs):
     """
-    Returns detection fraction for distribution of source sizes with log(major
-    axis) ~ N(loga, std_loga), minor-to-major axis distribution ~
-    Beta(alpha_e, beta_e), log(flux) at zero (u,v)-point distribution ~
-    N(mu_logS, tau_logS) and baseline length pdf ``bsl_pdf`` (e.g. from kernel
-    estimate).
+    :param name:
+        Distribution with percent point function (inverse of CDF) defined as
+        ``name.ppf(q)``, where ``q`` - lower tail probability.
+    :param cred_mass (optional):
+        Desired mass of HDI region.
+    :param tol (optioanl):
+        Passed to ``scipy.optimize.fmin`` with ``xtol`` keyword argument.
+    :param args:
+        Positional arguments to distribution ``name``.
+    :param kwargs:
+        Keyword arguments to distribution ``name``.
+    """
+    name = name(*args, **kwargs)
 
-    :param mu_loga:
-        Mean of normal distribution for loga, where a [mas].
-    :param std_loga:
-        Std of normal distribution for loga, where a [mas].
-    :param beta_e:
-    :param mu_logs:
-        Mean of normal distribution for logs, where s [Jy].
-    :param std_logs:
-        Std of normal distribution for logs, where s [Jy].
-    :param bsls:
-        Baselines. Array-like with size=``size``. If it is not, then resample
-        to ``size``.
-    :param alpha_e (optional):
-        Parameter of Beta-distribution for axis ratios. Default = 2.
-    :param s_thr:
-        Threshold flux for detection [Jy].
-    :param size:
-        Size of samples to draw.g
-    :return:
-    """
-    #prng = RandomState(123)
-    np.random.seed(123)
-    loga = np.random.normal(loc=mu_loga, scale=std_loga, size=size)
-    a = np.exp(loga)
-    e = np.random.beta(alpha_e, beta_e, size=size)
-    logs = np.random.normal(loc=mu_logs, scale=std_logs, size=size)
-    s = np.exp(logs)
-    if not len(bsls) == size:
-        bsls = np.random.choice(bsls, size=size)
-    pa = np.random.unif(0., np.pi, size=size)
-    fluxes = flux(bsls, pa, s, a, e)
-    n_det = len(np.where(fluxes > s_thr)[0])
-    return float(n_det) / size
+    def interval_width(low_tail_prob, name, cred_mass):
+        return name.ppf(cred_mass + low_tail_prob) - name.ppf(low_tail_prob)
+
+    hdi_low_tail_prob = fmin(interval_width, 0, (name, cred_mass,), xtol=tol)
+    return name.ppf(hdi_low_tail_prob), name.ppf(hdi_low_tail_prob + cred_mass)
