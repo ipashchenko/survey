34c127d99a124e1fad29e66555caf0af98c23050
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-10-14 19:22:16 +0400

    added models.ft_of_2d_gaussian()

--- survey/models.py
+++ survey/models.py
@@ -47,6 +47,10 @@ def flux_from_1_asymmetric_full(u, v, s_full, bmaj, e, bpa=0, x0=0, y0=0):
         Positional angle of major axis in image plane (from x to y) [rad].
     :param e:
         Ratio of minor to major axis of elliptical component.
+    :param x0:
+        Center of component in image plane [rad].
+    :param y0:
+        Center of component in image plane [rad].
     :return:
         Complex value of correlated flux at given uv-point.
     """
@@ -55,13 +59,81 @@ def flux_from_1_asymmetric_full(u, v, s_full, bmaj, e, bpa=0, x0=0, y0=0):
     v_ = -u * math.sin(bpa) + v * math.cos(bpa)
 
     bmin = bmaj * e
-    ft = s_full * math.pi * bmaj * bmin * np.exp(-math.pi ** 2 *
+    ft = s_full * math.pi * bmaj * bmin * np.exp(-math.pi ** 2 * 1j *
                                               (bmaj ** 2 * u_ ** 2 +
                                                bmin ** 2 * v_ ** 2))
     # Multiply on phases of ``x0``, ``y0`` in rotated system
     x0_ = x0 * math.cos(bpa) + y0 * math.sin(bpa)
     y0_= -x0 * math.sin(bpa) + y0 * math.cos(bpa)
     ft *= math.exp(2 * math.pi * 1j * (x0_ * u_ + y0_ * v_))
+
+    return ft
+
+
+def ft_of_2d_gaussian(u, v, s_full, bmaj, e, bpa=0, x0=0, y0=0):
+    """
+    Returns value of the Fourier transform of 2d elliptical gaussian with major
+    axis ``bmaj``, full flux ``s_full``, axis ratio ``e``, positional angle of
+    major axis ``bpa`` (counted from x-axis of image plane counter clockwise)
+    and center ``x0``, ``y0`` in image plane in point ``u``,
+    ``v`` of uv-plane.
+    :param u:
+    :param v:
+    :param s_full:
+    :param bmaj:
+    :param e:
+    :param bpa:
+    :param x0:
+    :param y0:
+    :return:
+    :notes:
+        The value of the Fourier transform of gaussian function (Wiki):
+
+            g(x, y) = A * exp[-(a*(x-x0)**2 + b*(x-x0)*(y-y0) + c*(y-y0)**2)]  (1)
+
+            where:
+
+                a = cos(\theta)**2/(2*std_x**2) + sin(\theta)**2/(2*std_y**2)
+                b = sin(2*\theta)/(2*std_x**2) - sin(2*\theta)/(2*std_y**2)
+                (corresponds to rotation counter clockwise)
+                c = sin(\theta)**2/(2*std_x**2) + cos(\theta)**2/(2*std_y**2)
+
+            for x0=0, y0=0 in point u,v of uv-plane is (Briggs Thesis):
+
+            2*pi*A*(4*a*c-b**2)**(-1/2)*exp[(4*pi**2/(4*a*c-b**2))*(-c*u**2+b*u*v-a*v**2)] (2)
+
+            As we parametrize the flux as ``s_full`` - full flux of gaussian
+            (that is at zero (u,v)-spacing), then change coefficient in front of
+            exponent to ``s_full``.
+
+            shift of (x0, y0) in image plane corresponds to phase shift in
+            uv-plane:
+
+            ft(x0,y0) = ft(x0=0,y0=0) * exp(-2*pi*(u*x0 + v*y0))
+    """
+    # Construct parameter of gaussian function (1)
+    std_x = bmaj
+    std_y = e * bmaj
+    theta = bpa
+    a = math.cos(theta) ** 2. / (2. * std_x ** 2.) +\
+        math.sin(theta) ** 2. / (2. * std_y ** 2.)
+    b = math.sin(2. * theta) / (2. * std_x ** 2.) -\
+        math.sin(2. * theta) / (2. * std_y ** 2.)
+    c = math.sin(theta) ** 2. / (2. * std_x ** 2.) + \
+        math.cos(theta) ** 2. / (2. * std_y ** 2.)
+    # Calculate the value of FT in point (u,v) for x0=0,y0=0 case using (2)
+    k = (4. * a * c - b ** 2.)
+    #ft = s_full * 2. * math.pi * k ** (-0.5) * math.exp((4. * math.pi ** 2. / k)
+    #                                                    * (-c * u ** 2. +
+    #                                                       b * u * v -
+    #                                                       a * v ** 2.))
+    ft = s_full * math.exp((4. * math.pi ** 2. / k) * (-c * u ** 2. +
+                                                       b * u * v -
+                                                       a * v ** 2.))
+    ft = complex(ft)
+    # If x0=!0 or y0=!0 then shift phase
+    if x0 or y0:
+        ft *= math.exp(-2. * math.pi * 1j * (u * x0 + v * y0))
 
     return ft
 
