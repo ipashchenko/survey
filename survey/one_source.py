bcc932f19ef3c40f1f947ebf9b265c20b91a43bf
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-12-22 15:56:20 +0300

    added one_source data in

--- survey/one_source.py
+++ survey/one_source.py
@@ -1,1 +1,14 @@
-__author__ = 'ilya'
+from load_data import get_array_from_dbtable
+
+
+def get_source_data(source_name, band):
+    """
+    Function that gets information about source detections/nondetections in RA
+    survey.
+    :param source_name:
+        Name of source.
+    :param band:
+        Band.
+    :return:
+    """
+    pass
